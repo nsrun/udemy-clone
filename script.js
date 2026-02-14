e1c7e82b49170297d877e171a5c792906916692a
Arunkumar <nsarun308@gmail.com>
2026-02-14 12:32:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var sideNavbar = document.querySelector(".side-navbar");
+
+function showSideNavbar() {
+    sideNavbar.style.left = "0";
+}
+
+function closeSideNavbar() {
+    sideNavbar.style.left = "-60%";
+}
